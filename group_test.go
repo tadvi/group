8cfbde4efc7319fa68c42faee43d4870f84e32b7
Tad Vizbaras <tad@etasoft.com>
2019-04-11 10:43:17 -0400

    add more tests

--- group_test.go
+++ group_test.go
@@ -40,10 +40,33 @@ func TestOne(t *testing.T) {
     go func() { res <- g.Run(ctx) }()
     select {
     case errs := <-res:
-        if want, got := myError, errs; len(got) == 0 || want != got[0] {
+        if want, got := myError, errs; len(got) != 1 || want != got[0] {
             t.Errorf("got %v, want %v", got, want)
         }
     case <-time.After(100 * time.Millisecond):
         t.Error("timeout")
+    }
+}
+
+func TestMany(t *testing.T) {
+    myError := errors.New("foobar")
+    var g group.Tasks
+
+    ctx, cancel := context.WithCancel(context.Background())
+    defer cancel()
+
+    g.Add(func(ctx context.Context) error { return myError })
+    g.Add(func(ctx context.Context) error { return myError })
+    g.Add(func(ctx context.Context) error { return nil })
+
+    res := make(chan []error)
+    go func() { res <- g.Run(ctx) }()
+    select {
+    case errs := <-res:
+        if want, got := myError, errs; len(got) != 2 || want != got[0] || want != got[1] {
+            t.Errorf("got %v, want %v", got, want)
+        }
+    case <-time.After(100 * time.Millisecond):
+        t.Errorf("timeout")
     }
 }
